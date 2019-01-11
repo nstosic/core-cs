6f91e5485ac2fcbbce9e7a5cdaf98ba3197417d9
nstosic <nemanjastosic@outlook.com>
2019-01-11 14:32:37 +0100

    Additional unit tests for String

--- data-structures/test/string.cpp
+++ data-structures/test/string.cpp
@@ -1,13 +1,48 @@
 #include <gtest/gtest.h>
 #include "../src/string.h"
 
-TEST(StackSuite, NewStringIsEmpty) {
+TEST(StringSuite, NewStringIsEmpty) {
     // Setup
     String softwareUnderTest;
 
     // Execution
-    int count = softwareUnderTest.length();
+    unsigned long count = softwareUnderTest.length();
 
     // Verification
-    ASSERT_EQ(0, count);
+    ASSERT_EQ(0L, count);
+}
+
+TEST(StringSuite, UnsafePointerReturnsCorrectAddress) {
+    // Setup
+    String *softwareUnderTest = new String();
+    char *pointer = (char *)softwareUnderTest;
+    // Manually fill memory location for String::buffer
+    *pointer = 0x01;
+    *(pointer + 1) = 0x02;
+    *(pointer + 2) = 0x03;
+    *(pointer + 3) = 0x04;
+    unsigned long address;
+
+    // Execution
+    address = (unsigned long)softwareUnderTest->unsafePointer();
+
+    // Verification
+    ASSERT_EQ(0x04030201, address);
+}
+
+TEST(StringSuite, RValueIsAssignedToLValueInEqualOperator) {
+    // Setup
+    const char *testString = "test";
+    String softwareUnderTest;
+
+    // Execution
+    softwareUnderTest = testString;
+
+    // Verification
+    for (unsigned long i = 0; i < sizeof(testString); i++) {
+        ASSERT_EQ(
+            (unsigned char)*(testString + i),
+            (unsigned char)*(softwareUnderTest.unsafePointer() + i)
+            );
+    }
 }
