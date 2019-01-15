c7d22c5df995592184d7ef0e7e8716ccf1798790
nstosic <nemanjastosic@outlook.com>
2019-01-15 17:48:23 +0100

    Fixed off by one error when adding new symbol to dictionary

--- algorithms/src/lzwcompression.cpp
+++ algorithms/src/lzwcompression.cpp
@@ -15,7 +15,7 @@ namespace lzw {
 
     int compression_step(std::string input, std::unordered_map<std::string, int> &current_dictionary) {
         if (current_dictionary.find(input) == current_dictionary.end()) {
-            current_dictionary[input] = (int)current_dictionary.size() - 1;
+            current_dictionary[input] = (int)current_dictionary.size();
             return current_dictionary[input.substr(0, input.size() - 1)];
         }
         return -1;
