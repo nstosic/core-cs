f05d28ed447272ce88dbc64eae81258eac5e96e3
nstosic <nemanjastosic@outlook.com>
2019-02-13 22:28:38 +0100

    Wrote additional tests for BST

--- data-structures/test/binary_search_tree.cpp
+++ data-structures/test/binary_search_tree.cpp
@@ -11,3 +11,61 @@ TEST(BinarySearchTreeSuite, InitialTreeIsEmpty) {
     // Verification
     ASSERT_EQ(nullptr, root);
 }
+
+TEST(BinarySearchTreeeSuite, NodeAddedToEmptyTreeBecomesItsRoot) {
+    // Setup
+    BinarySearchTree<int> software_under_test;
+
+    // Execution
+    software_under_test.Insert(2);
+
+    // Verification
+    ASSERT_EQ(2, software_under_test.GetRoot()->GetData());
+}
+
+TEST(BinarySearchTreeSuite, ValueAddedToRootBecomesItsLeftChildIfSmaller) {
+    // Setup
+    BinarySearchTree<int> software_under_test;
+    software_under_test.Insert(5);
+
+    // Execution
+    software_under_test.Insert(2);
+
+    // Verification
+    ASSERT_EQ(2, software_under_test.GetRoot()->GetChildren()[0]->GetData());
+}
+
+TEST(BinarySearchTreeSuite, ValueAddedToRootBecomesItsRightChildIfGreater) {
+    // Setup
+    BinarySearchTree<int> software_under_test;
+    software_under_test.Insert(5);
+
+    // Execution
+    software_under_test.Insert(22);
+
+    // Verification
+    ASSERT_EQ(22, software_under_test.GetRoot()->GetChildren()[1]->GetData());
+}
+
+TEST(BinarySearchTreeSuite, InsertReturnsFalseIfValueAlreadyExistsInTheTree) {
+    // Setup
+    BinarySearchTree<int> software_under_test;
+    software_under_test.Insert(5);
+
+    // Execution
+    bool test = software_under_test.Insert(5);
+
+    // Verification
+    ASSERT_FALSE(test);
+}
+
+TEST(BinarySearchTreeSuite, RemovingFromEmptyTreeReturnsFalse) {
+    // Setup
+    BinarySearchTree<int> software_under_test;
+
+    // Execution
+    bool test = software_under_test.Remove(2);
+
+    // Verification
+    ASSERT_FALSE(test);
+}
