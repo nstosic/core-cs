96ecfe514f2bcc77aae3910b4701b2cea0a609c5
nstosic <nemanjastosic@outlook.com>
2019-02-14 00:26:01 +0100

    Fixed syntax error

---
+++ data-structures/test/abstract_tree.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+#include "../src/abstract_tree.h"
+
+TEST(AbstractTreeSuite, DefaultConstructorCreatesEmptyTree) {
+    // Setup
+    // AbstractTree<int> software_under_test;
+
+    // Execution
+
+
+    // Verification
+}
