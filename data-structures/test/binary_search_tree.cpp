21e505d82d08b51e713912197ae28286e9bd1dbf
nstosic <nemanjastosic@outlook.com>
2019-02-14 17:14:58 +0100

    Wrote additional tests for BST

--- data-structures/test/binary_search_tree.cpp
+++ data-structures/test/binary_search_tree.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 #include "../src/binary_search_tree.h"
+#include <random>
+#include <queue>
 
 TEST(BinarySearchTreeSuite, InitialTreeIsEmpty) {
     // Setup
@@ -68,4 +70,100 @@ TEST(BinarySearchTreeSuite, RemovingFromEmptyTreeReturnsFalse) {
 
     // Verification
     ASSERT_FALSE(test);
+}
+
+TEST(BinarySearchTreeSuite, InsertingArbitraryValuesPreservesBSTPropertyForImmediateChildren) {
+    // Setup
+    BinarySearchTree<int> software_under_test;
+    std::random_device random;
+    std::mt19937 generator(random());
+    std::uniform_int_distribution<> random_initializer(-100, 100);
+    
+    // Execution
+    for (int i = 0; i < 15; i++) {
+        software_under_test.Insert(random_initializer(generator));
+    }
+
+    // Verification
+    std::queue<TreeNode<int>*> inorder_queue;
+    inorder_queue.push(software_under_test.GetRoot());
+    TreeNode<int> *ptr;
+    while (!inorder_queue.empty()) {
+        ptr = inorder_queue.front();
+        inorder_queue.pop();
+        if (ptr->GetChildren()[0] != nullptr) {
+            ASSERT_TRUE(ptr->GetData() > ptr->GetChildren()[0]->GetData());
+        }
+        if (ptr->GetChildren()[1] != nullptr) {
+            ASSERT_TRUE(ptr->GetData() < ptr->GetChildren()[1]->GetData());
+        }
+    }
+}
+
+TEST(BinarySearchTreeSuite, SearchReturnsNullptrIfTheTreeIsEmpty) {
+    // Setup
+    BinarySearchTree<int> software_under_test;
+
+    // Execution
+    TreeNode<int>* test = software_under_test.Search(2);
+
+    // Verification
+    ASSERT_EQ(nullptr, test);
+}
+
+TEST(BinarySearchTreeSuite, SearchReturnsCorrectTreeNode) {
+    // Setup
+    BinarySearchTree<int> software_under_test;
+    software_under_test.Insert(1);
+    software_under_test.Insert(2);
+    software_under_test.Insert(3);
+
+    // Execution
+    TreeNode<int>* test = software_under_test.Search(2);
+
+    // Verification
+    ASSERT_EQ(software_under_test.GetRoot()->GetChildren()[1], test);
+}
+
+TEST(BinarySearchTreeSuite, RemoveReturnsFalseWhenCalledOnAnEmptyTree) {
+    // Setup
+    BinarySearchTree<int> software_under_test;
+
+    // Execution
+    bool test = software_under_test.Remove(2);
+
+    // Verification
+    ASSERT_FALSE(test);
+}
+
+TEST(BinarySearchTreeSuite, RemoveReturnsTrueIfTheDataIsFoundInTheTree) {
+    // Setup
+    BinarySearchTree<int> software_under_test;
+    software_under_test.Insert(2);
+    software_under_test.Insert(3);
+    software_under_test.Insert(4);
+    software_under_test.Insert(1);
+
+    // Execution
+    bool test = software_under_test.Remove(3);
+
+    // Verification
+    ASSERT_TRUE(test);
+}
+
+TEST(BinarySearchTreeSuite, RemovePreservesTreeStructureIfTheDataIsFoundInTheRoot) {
+    // Setup
+    BinarySearchTree<int> software_under_test;
+    software_under_test.Insert(2);
+    software_under_test.Insert(3);
+    software_under_test.Insert(4);
+    software_under_test.Insert(1);
+
+    // Execution
+    software_under_test.Remove(2);
+
+    // Verification
+    ASSERT_EQ(1, software_under_test.GetRoot()->GetData());
+    ASSERT_EQ(3, software_under_test.GetRoot()->GetChildren()[1]->GetData());
+    ASSERT_EQ(4, software_under_test.GetRoot()->GetChildren()[1]->GetChildren()[1]->GetData());
 }
