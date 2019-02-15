d5c3f81a816b0974a492c75b6b14d75297bfb3ca
nstosic <nemanjastosic@outlook.com>
2019-02-15 16:54:36 +0100

    Bugfixing in BST Remove method

--- data-structures/src/binary_search_tree.cpp
+++ data-structures/src/binary_search_tree.cpp
@@ -1,4 +1,5 @@
 #include "binary_search_tree.h"
+#include <assert.h>
 
 template <typename T>
 TreeNode<T>* BinarySearchTree<T>::Search(T data) const {
@@ -23,14 +24,14 @@ bool BinarySearchTree<T>::Insert(T data) {
     while (temp->GetNumberOfChildren() != 0) {
         if (temp->GetData() < data) {
             if (temp->GetChildren()[1] == nullptr) {
-                temp->AddChild(new TreeNode<T>(data, BST_NUMBER_OF_CHILDREN), 1);
+                temp->SetChild(new TreeNode<T>(data, BST_NUMBER_OF_CHILDREN), 1);
                 return true;
             } else {
                 temp = temp->GetChildren()[1];
             }
         } else if (temp->GetData() > data) {
             if (temp->GetChildren()[0] == nullptr) {
-                temp->AddChild(new TreeNode<T>(data, BST_NUMBER_OF_CHILDREN), 0);
+                temp->SetChild(new TreeNode<T>(data, BST_NUMBER_OF_CHILDREN), 0);
                 return true;
             } else {
                 temp = temp->GetChildren()[0];
@@ -41,10 +42,10 @@ bool BinarySearchTree<T>::Insert(T data) {
         }
     }
     if (temp->GetData() < data) {
-        temp->AddChild(new TreeNode<T>(data, BST_NUMBER_OF_CHILDREN), 1);
+        temp->SetChild(new TreeNode<T>(data, BST_NUMBER_OF_CHILDREN), 1);
         return true;
     } else if (temp->GetData() > data) {
-        temp->AddChild(new TreeNode<T>(data, BST_NUMBER_OF_CHILDREN), 0);
+        temp->SetChild(new TreeNode<T>(data, BST_NUMBER_OF_CHILDREN), 0);
         return true;
     } else {
         // If temp->GetData() == data
@@ -54,12 +55,16 @@ bool BinarySearchTree<T>::Insert(T data) {
 
 template <typename T>
 bool BinarySearchTree<T>::Remove(T data) {
+    if (this->root_ == nullptr) {
+        return false;
+    }
+    if (this->root_->GetData() == data && this->root_->GetNumberOfChildren() == 0) {
+        delete this->root_;
+        this->root_ = nullptr;
+    }
     TreeNode<T>* parent = nullptr;
     TreeNode<T>* temp = this->root_;
     unsigned int index = 2;
-    if (temp == nullptr) {
-        return false;
-    }
     while (temp->GetNumberOfChildren() != 0) {
         if (temp->GetData() == data) {
             break;
@@ -72,67 +77,66 @@ bool BinarySearchTree<T>::Remove(T data) {
             if (temp->GetChildren()[index] == nullptr) {
                 return false;
             } else {
+                parent = temp;
                 temp = temp->GetChildren()[index];
             }
         }
     }
     if (temp->GetData() == data) {
-        deleteInPlace(temp, parent, index);
+        internalDelete(temp, parent, index);
         return true;
     }
     return false;
 }
 
 template <typename T>
-inline void deleteInPlace(TreeNode<T>* node_to_delete, TreeNode<T>* parent, unsigned int child_index) {
-    TreeNode<T>* replacement = nullptr;
-    TreeNode<T>* replacement_parent = nullptr;
-    if (parent == nullptr) {
-        // node_to_delete is the root of the tree
-        replacement_parent = node_to_delete;
-        replacement = node_to_delete->GetChildren()[0];
-        if (replacement != nullptr) {
-            while (replacement->GetChildren()[1] != nullptr) {
-                replacement_parent = replacement;
-                replacement = replacement->GetChildren()[1];
+inline void internalDelete(TreeNode<T>* node_to_delete, TreeNode<T>* parent, unsigned int child_index) {
+    if (node_to_delete->GetNumberOfChildren() == 0) {
+        // Case #1: Node that needs to be deleted has no children
+        parent->SetChild(nullptr, child_index);
+        delete node_to_delete;
+    } else if (node_to_delete->GetNumberOfChildren() == 1) {
+        // Case #2: Node that needs to be deleted has one child
+        if (parent == nullptr) {
+            // Node being deleted is the root of the tree
+            parent = node_to_delete;
+            if (node_to_delete->GetChildren()[0] != nullptr) {
+                node_to_delete = node_to_delete->GetChildren()[0];
+            } else {
+                node_to_delete = node_to_delete->GetChildren()[1];
             }
-            replacement_parent->AddChild(replacement->GetChildren()[0], 1);
-            node_to_delete->SetData(replacement->GetData());
-            delete replacement;
+            parent->SetData(node_to_delete->GetData());
+            parent->SetChild(node_to_delete->GetChildren()[0], 0);
+            parent->SetChild(node_to_delete->GetChildren()[1], 1);
+            delete node_to_delete;
         } else {
-            replacement = node_to_delete->GetChildren()[1];
-            if (replacement != nullptr) {
-                while (replacement->GetChildren()[0] != nullptr) {
-                    replacement_parent = replacement;
-                    replacement = replacement->GetChildren()[0];
-                }
-                replacement_parent->AddChild(replacement->GetChildren()[1], 0);
-                node_to_delete->SetData(replacement->GetData());
-                delete replacement;
+            if (node_to_delete->GetChildren()[0] != nullptr) {
+                parent->SetChild(node_to_delete->GetChildren()[0], child_index);
+            } else {
+                parent->SetChild(node_to_delete->GetChildren()[1], child_index);
             }
+            delete node_to_delete;
         }
-    } else if (child_index == 0) {
-        // Look for the greatest value in node_to_delete subtree
-        replacement_parent = parent;
-        replacement = node_to_delete;
-        while (replacement->GetChildren()[1] != nullptr) {
-            replacement_parent = replacement;
-            replacement = replacement->GetChildren()[1];
-        }
-        replacement_parent->AddChild(replacement->GetChildren()[0], 1);
-        node_to_delete->SetData(replacement->GetData());
-        delete replacement;
     } else {
-        // Look for the smallest value in node_to_delete subtree
-        replacement_parent = parent;
-        replacement = node_to_delete;
-        while (replacement->GetChildren()[0] != nullptr) {
-            replacement_parent = replacement;
-            replacement = replacement->GetChildren()[0];
+        // Case #3: Node that needs to be deleted has both children
+        TreeNode<T>* inorder_successor = node_to_delete->GetChildren()[1];
+        TreeNode<T>* inorder_successor_parent = node_to_delete;
+        unsigned int parent_index = 1;
+        while (inorder_successor->GetChildren()[0] != nullptr) {
+            inorder_successor_parent = inorder_successor;
+            inorder_successor = inorder_successor->GetChildren()[0];
+            parent_index = 0;
         }
-        replacement_parent->AddChild(replacement->GetChildren()[1], 0);
-        node_to_delete->SetData(replacement->GetData());
-        delete replacement;
+        inorder_successor_parent->SetChild(inorder_successor->GetChildren()[1], parent_index);
+        if (parent != nullptr) {
+            parent->SetChild(inorder_successor, child_index);
+            inorder_successor->SetChild(node_to_delete->GetChildren()[0], 0);
+            inorder_successor->SetChild(node_to_delete->GetChildren()[1], 1);
+            delete node_to_delete;
+        } else {
+            node_to_delete->SetData(inorder_successor->GetData());
+            delete inorder_successor;
+        }
     }
 }
 
