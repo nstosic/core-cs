f5adce84558366a7833ca4f7923fd14f14b9f85f
nstosic <nemanjastosic@outlook.com>
2019-03-08 16:27:41 +0100

    Additional unit tests for LRU cache

--- data-structures/src/lru_cache.h
+++ data-structures/src/lru_cache.h
@@ -4,6 +4,8 @@
 #include <unordered_map>
 #include "double_linked_node.h"
 
+class ElementNotFoundException: public std::exception {};
+
 template <typename T>
 class LruCache {
 private:
@@ -14,6 +16,8 @@ private:
     std::unordered_map<int, T> cache_;
     LruCache(unsigned int capacity, unsigned int size, DoubleLinkedNode<int>* first, DoubleLinkedNode<int>* last);
     void moveToFront(int key);
+    friend class LruCacheSuite_GetMovesTheObjectToTheFrontOfTheRecentlyUsedList_Test;
+    friend class LruCacheSuite_PutPlacesTheObjectToTheFrontOfTheRecentlyUsedList_Test;
 public:
     LruCache();
     LruCache(unsigned int capacity);
--- data-structures/test/lru_cache.cpp
+++ data-structures/test/lru_cache.cpp
@@ -10,9 +10,6 @@ TEST(LruCacheSuite, EmptyConstructorCreatesCacheWithCapacityOfOne) {
     // Verification
     char* cache_ptr = (char*)&software_under_test;
     ASSERT_EQ(1, *((int*)(void*)cache_ptr)); // capacity
-    ASSERT_EQ(0, *((int*)(void*)(cache_ptr + sizeof(unsigned int)))); // size
-    ASSERT_EQ(nullptr, *((DoubleLinkedNode<int>**)(void*)(cache_ptr + sizeof(unsigned int) * 2))); // first
-    ASSERT_EQ(nullptr, *((DoubleLinkedNode<int>**)(void*)(cache_ptr + sizeof(unsigned int) * 2 + sizeof(DoubleLinkedNode<int>*)))); // last
 }
 
 TEST(LruCacheSuite, ConstructorWithFixedCapacityCreatesCacheWithGivenCapacity) {
@@ -24,9 +21,46 @@ TEST(LruCacheSuite, ConstructorWithFixedCapacityCreatesCacheWithGivenCapacity) {
     // Verification
     char* cache_ptr = (char*)&software_under_test;
     ASSERT_EQ(5, *((int*)(void*)cache_ptr)); // capacity
-    ASSERT_EQ(0, *((int*)(void*)(cache_ptr + sizeof(unsigned int)))); // size
-    ASSERT_EQ(nullptr, *((DoubleLinkedNode<int>**)(void*)(cache_ptr + sizeof(unsigned int) * 2))); // first
-    ASSERT_EQ(nullptr, *((DoubleLinkedNode<int>**)(void*)(cache_ptr + sizeof(unsigned int) * 2 + sizeof(DoubleLinkedNode<int>*)))); // last
+}
+
+TEST(LruCacheSuite, GetReturnsTheObjectCorrespondingToTheKey) {
+    // Setup
+    LruCache<int> software_under_test(6);
+    software_under_test.put(3, 33);
+
+    // Execution
+    int test = software_under_test.get(3);
+
+    // Verification
+    ASSERT_EQ(33, test);
+}
+
+TEST(LruCacheSuite, GetMovesTheObjectToTheFrontOfTheRecentlyUsedList) {
+    // Setup
+    LruCache<int> software_under_test(6);
+    software_under_test.put(3, 33);
+    software_under_test.put(1, 11);
+    software_under_test.put(2, 22);
+
+    // Execution
+    software_under_test.get(3);
+    software_under_test.get(2);
+    software_under_test.get(1);
+    software_under_test.get(3);
+    
+    // Verification
+    ASSERT_EQ(3, software_under_test.first_->getData());
+    ASSERT_EQ(2, software_under_test.last_->getData());
+}
+
+TEST(LruCacheSuite, GetThrowsAnExceptionIfNoObjectForTheGivenKeyExistsInTheCache) {
+    // Setup
+    LruCache<int> software_under_test(6);
+
+    // Execution
+
+    // Verification
+    ASSERT_THROW(software_under_test.get(6), ElementNotFoundException);
 }
 
 TEST(LruCacheSuite, PutAddsTheValueToTheUnderlyingCacheDataStructure) {
@@ -40,4 +74,75 @@ TEST(LruCacheSuite, PutAddsTheValueToTheUnderlyingCacheDataStructure) {
     char* cache_ptr = (char*)&software_under_test;
     std::unordered_map<int, int>* underlying_cache = (std::unordered_map<int, int>*)(void*)(cache_ptr + sizeof(unsigned int) * 2 + sizeof(DoubleLinkedNode<int>*) * 2);
     ASSERT_TRUE(underlying_cache->count(5) > 0);
+}
+
+TEST(LruCacheSuite, PutPlacesTheObjectToTheFrontOfTheRecentlyUsedList) {
+    // Setup
+    LruCache<int> software_under_test(6);
+
+    // Execution
+    software_under_test.put(4, 4);
+    software_under_test.put(3, 3);
+    software_under_test.get(4);
+    software_under_test.put(5, 5);
+    software_under_test.get(4);
+    software_under_test.put(6, 6);
+    software_under_test.put(66, 66);
+
+    // Verification
+    ASSERT_EQ(66, software_under_test.first_->getData());
+}
+
+TEST(LruCacheSuite, PutDoesNotRemoveAnyCachedObjectsIfCacheIsNotFull) {
+    // Setup
+    LruCache<int> software_under_test(6);
+    software_under_test.put(5, 6);
+    software_under_test.put(4, 5);
+
+    // Execution
+    software_under_test.put(6, 6);
+
+    // Verification
+    char* cache_ptr = (char*)&software_under_test;
+    std::unordered_map<int, int>* underlying_cache = (std::unordered_map<int, int>*)(void*)(cache_ptr + sizeof(unsigned int) * 2 + sizeof(DoubleLinkedNode<int>*) * 2);
+    ASSERT_TRUE(underlying_cache->count(4) > 0);
+    ASSERT_TRUE(underlying_cache->count(5) > 0);
+}
+
+TEST(LruCacheSuite, PutRemovesLastRecentlyUsedObjectIfCacheIsFull) {
+    // Setup
+    LruCache<int> software_under_test(2);
+    software_under_test.put(5, 6);
+    software_under_test.put(4, 5);
+
+    // Execution
+    software_under_test.put(6, 6);
+
+    // Verification
+    char* cache_ptr = (char*)&software_under_test;
+    std::unordered_map<int, int>* underlying_cache = (std::unordered_map<int, int>*)(void*)(cache_ptr + sizeof(unsigned int) * 2 + sizeof(DoubleLinkedNode<int>*) * 2);
+    ASSERT_TRUE(underlying_cache->count(4) > 0);
+    ASSERT_TRUE(underlying_cache->count(5) == 0);
+}
+
+TEST(LruCacheSuite, CapacityReturnsProperValue) {
+    // Setup
+    LruCache<int> software_under_test(3);
+
+    // Execution
+    unsigned int capacity = software_under_test.capacity();
+
+    // Verification
+    ASSERT_EQ(3, capacity);
+}
+
+TEST(LruCacheSuite, CapacityReturnsZeroForNewlyCreatedLruCache) {
+    // Setup
+    LruCache<int> software_under_test(3);
+
+    // Execution
+    unsigned int size = software_under_test.size();
+
+    // Verification
+    ASSERT_EQ(0, size);
 }
