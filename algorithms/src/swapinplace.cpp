fe9af9ef8e5a6b40b39abfe1d263f65baeff92d3
nstosic <nemanjastosic@outlook.com>
2019-03-15 10:28:54 +0100

    Added XOR based swap algorithm that works in place even for large input values

--- algorithms/src/swapinplace.cpp
+++ algorithms/src/swapinplace.cpp
@@ -1,6 +1,12 @@
-void SwapInPlace(int &x, int &y) {
+void SwapInPlaceArithmetical(int &x, int &y) {
     // This algorithm is prone to fail when parameters have big absolute values because of overflow issues.
     x = x + y;
     y = x - y;
     x = x - y;
+}
+
+void SwapInPlaceXor(int &x, int &y) {
+    x = x ^ y;
+    y = x ^ y;
+    x = x ^ y;
 }
--- algorithms/test/swapinplace.cpp
+++ algorithms/test/swapinplace.cpp
@@ -1,25 +1,70 @@
 #include <gtest/gtest.h>
 
-extern void SwapInPlace(int &x, int &y);
+extern void SwapInPlaceArithmetical(int &x, int &y);
+extern void SwapInPlaceXor(int &x, int &y);
 
-TEST(SwapInPlaceSuite, VerifyFirstArgumentValueAfterOperation) {
+TEST(SwapInPlaceSuite, VerifyFirstArgumentValueAfterArithmeticalSwapOperation) {
     // Setup
     int x = 3, y = 6;
 
     // Execution
-    SwapInPlace(x, y);
+    SwapInPlaceArithmetical(x, y);
 
     // Verification
     ASSERT_EQ(6, x);
 }
 
-TEST(SwapInPlaceSuite, VerifySecondArgumentValueAfterOperation) {
+TEST(SwapInPlaceSuite, VerifySecondArgumentValueAfterArithmeticalSwapOperation) {
     // Setup
     int x = 3, y = 6;
 
     // Execution
-    SwapInPlace(x, y);
+    SwapInPlaceArithmetical(x, y);
 
     // Verification
     ASSERT_EQ(3, y);
+}
+
+TEST(SwapInPlaceSuite, VerifyFirstArgumentValueAfterLogicalSwapOperation) {
+    // Setup
+    int x = 3, y = 6;
+
+    // Execution
+    SwapInPlaceXor(x, y);
+
+    // Verification
+    ASSERT_EQ(6, x);
+}
+
+TEST(SwapInPlaceSuite, VerifySecondArgumentValueAfterLogicalSwapOperation) {
+    // Setup
+    int x = 3, y = 6;
+
+    // Execution
+    SwapInPlaceXor(x, y);
+
+    // Verification
+    ASSERT_EQ(3, y);
+}
+
+TEST(SwapInPlaceSuite, VerifyFirstArgumentValueAfterLogicalSwapOperationForLargeInput) {
+    // Setup
+    int x = 2147483647, y = -2147483648; // 2 ^ 31 = 2147483648
+
+    // Execution
+    SwapInPlaceXor(x, y);
+
+    // Verification
+    ASSERT_EQ(-2147483648, x);
+}
+
+TEST(SwapInPlaceSuite, VerifySecondArgumentValueAfterLogicalSwapOperationForLargeInput) {
+    // Setup
+    int x = 2147483647, y = -2147483648; // 2 ^ 31 = 2147483648
+
+    // Execution
+    SwapInPlaceXor(x, y);
+
+    // Verification
+    ASSERT_EQ(2147483647, y);
 }
