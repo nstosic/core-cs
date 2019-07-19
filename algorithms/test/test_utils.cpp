35475e6a069fd8c749c99de788fbff79824d0970
nstosic <nemanjastosic@outlook.com>
2019-07-19 09:16:01 +0200

    Added initializeList utility function for unit tests

--- algorithms/test/test_utils.cpp
+++ algorithms/test/test_utils.cpp
@@ -1,4 +1,5 @@
 #include <cstdarg>
+#include "../../data-structures/src/list.h"
 
 int* initializeArray(const unsigned int param_count...) {
     int* array = new int[param_count];
@@ -9,4 +10,15 @@ int* initializeArray(const unsigned int param_count...) {
     }
     va_end(args);
     return array;
+}
+
+List<int>* initializeList(const unsigned int param_count...) {
+    List<int>* list = new List<int>();
+    va_list args;
+    va_start(args, param_count);
+    for (unsigned int i = 0; i < param_count; i++) {
+        list->Add(va_arg(args, int));
+    }
+    va_end(args);
+    return list;
 }
